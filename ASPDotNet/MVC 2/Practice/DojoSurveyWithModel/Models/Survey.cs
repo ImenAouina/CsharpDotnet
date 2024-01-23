5cb74898591ab600d13ef34a9d8f76f45eea0309
unknown <aouinaimen@gmail.com>
2024-01-23 10:34:41 +0100

    Dojo Survey with Model

---
+++ ASPDotNet/MVC 2/Practice/DojoSurveyWithModel/Models/Survey.cs
@@ -0,0 +1,11 @@
+#pragma warning disable CS8618
+namespace DojoSurveyWithModel.Models;
+public class Survey
+{
+    // your Survey fields go here
+    public string Name {get; set;}
+    public string Location {get; set;}
+    public string Language {get; set;}
+    public string Comment {get; set;}
+}
+
